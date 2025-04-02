cc7a7bd24fbf41fa1d4960306b05d7c29a48d0a5
Andrii Kyrch <andrii.kyrch@gmail.com>
2025-04-02 19:47:56 +0200

    Make homework 2

---
+++ src/components/App.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Description from './Description/Description.jsx';
+import Options from './Options/Options.jsx';
+import Feedback from './Feedback/Feedback.jsx';
+import Notification from './Notification/Notification.jsx';
+
+function App() {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('feedback');
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : {
+          good: 0,
+          neutral: 0,
+          bad: 0,
+        };
+  });
+
+  useEffect(() => {
+    localStorage.setItem('feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+
+  return (
+    <>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
+}
+
+export default App;
